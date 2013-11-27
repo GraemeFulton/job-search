485a44627e3bcfddbc5d78121c368b8dd98e4806
Graeme Fulton <graemefulton89@gmail.com>
2013-11-27 22:44:57 +0000

    Filter message when no matches, and updated url for linked companies/unis on posts
    Former-commit-id: f7494a9407678b67f62cec4434c5396e7ebef963

--- wp-content/plugins/display-taxonomy/Views/course_post_loop.php
+++ wp-content/plugins/display-taxonomy/Views/course_post_loop.php
@@ -1,10 +1,14 @@
 <!--course post loop template-->
 	
 <?php  
-$tree= display_taxonomy_tree('subject', 'uni');
+if (!have_posts()){echo '<h2 class="hentry" style="width:100%"><br><br> Hey, we don&apos;t have any courses matching this criteria at the moment, please try a different filter.</h2>';
+return;
+}
 ?>
 
 			<?php if (have_posts()) : while (have_posts()) : the_post();
+                        $tree= display_taxonomy_tree('subject', 'uni');
+
                             $post_id=get_the_ID();
                         ?>
                     
