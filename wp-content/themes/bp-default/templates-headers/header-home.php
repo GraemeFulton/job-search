ea051696113808096ea136c7033c686251432f28
Graeme <Graemefulton89@gmail.com>
2014-01-13 04:31:32 +0000

    added basic homepage
    Former-commit-id: 84d4860cf32cfe5f0ec878ab391f85ad86c7599f

---
+++ wp-content/themes/bp-default/templates-headers/header-home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+	<head profile="http://gmpg.org/xfn/11">
+		<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
+		<?php if ( current_theme_supports( 'bp-default-responsive' ) ) : ?><meta name="viewport" content="width=device-width, initial-scale=1.0" /><?php endif; ?>
+		<title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+		<?php bp_head(); ?>
+		<?php wp_head(); ?>
+                <?php header("HTTP/1.1 200 OK");?>
+	</head>
+
+	<body <?php body_class(); ?> id="bp-default">
+      <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+<a class="navbar-brand" href="<?php bloginfo('url')?>"><?php bloginfo('name')?></a>        </div>
+        <div class="navbar-collapse collapse">
+         <?php /* Primary navigation */
+   wp_nav_menu( array(
+        'menu'              => 'top_menu',
+        'theme_location'    => 'primary',
+        'depth'             => 2,
+        'container'         => false,
+        'container_class'   => 'collapse navbar-collapse navbar-ex1-collapse',
+        'menu_class'        => 'nav navbar-nav',
+        'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+        'walker'            => new wp_bootstrap_navwalker())
+    );
+
+?>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+		<?php do_action( 'bp_before_header' ); ?>
+
+		<div id="header">                  
+                    <div id="search-bar" role="search">
+                            <div id="main_search">
+                            <?php get_search_form(); ?>
+                            </div>
+			</div>
+			<!-- #search-bar -->
+                          
+                    			<?php do_action( 'bp_header' );?>
+
+		</div>
+		<!-- #header -->
+
+		<?php do_action( 'bp_after_header'     ); ?>
+		<?php do_action( 'bp_before_container' ); ?>
+                  <div class="slider-button fixed"></div>
+
+		<div id="container">
--- wp-content/themes/bp-default/templates-pages/page-bookmarks.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Template Name: Bookmarks (All)
-*
-* A Page for bookmarked posts
-*/
-echo "Bookmark Page";
-
-?>
