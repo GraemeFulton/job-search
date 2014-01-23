cc83d34fe292a670acc1b18f961fc9efbd7504a6
Graeme <Graemefulton89@gmail.com>
2014-01-23 07:00:52 +0000

    homepage login and registration widget
    Former-commit-id: 3e205cd3bcb7a99ccc409f6b275fd77e22c378b6

--- wp-content/themes/bp-default/templates-pages/page-home.php
+++ wp-content/themes/bp-default/templates-pages/page-home.php
@@ -27,16 +27,17 @@ include (TEMPLATEPATH . '/templates-headers/header-home.php');
 		
 		<!-- description -->
 		<div class="home-tag-line">
-		<h2><b>Start Building Your Brighter Future</b></h2>
-						<h4>Lostgrad is a free service, offering opportunities to help you unlock your full potential, and create the life you want.</h4>
+		<h2><b>Start Building Your Brighter Future, For Free.</b></h2>
+						<h4>Lostgrad is a free service, offering opportunities to help you unlock your true potential, and create the life you want.</h4>
 		<div class="home-show-case">
-		<h4><i class="ico fa fa-book"></i> Take World Class Courses, for Free</h4>
+		<h4><i class="ico fa fa-book"></i> Take Free World Class Courses</h4>
 		<h4><i class="ico fa fa-plane"></i> Experience the World</h4>
 		<h4><i class="ico fa fa-crosshairs"></i> Discover Your Perfect Job</h4>
 		<h4><i class="ico fa fa-comments"></i> Engage in Discussions</h4>
 		</div>
 		<br>
 		
+				<?php echo do_shortcode('[widgets_on_pages id="Login Widget"]'); ?>
 		
 		</div>
 		
@@ -48,10 +49,21 @@ include (TEMPLATEPATH . '/templates-headers/header-home.php');
         <div class="featured-area">
         
         <div id="Members_Feed">
-        <h2><i class="ico fa fa-users"></i> Members Activity</h2>
+        <h2><i class="ico fa fa-users"></i> Join Our Community</h2>
+        
+<h4><i class="ico fa fa-user"></i> Members</h4>
+<?php 
+echo do_shortcode('[widgets_on_pages id="Members Box"]');
+?>
+<h4><i class="ico fa fa-users"></i> Groups</h4>
+<?php 
+echo do_shortcode('[widgets_on_pages id="Groups Box"]');
+?>
+<h4><i class="ico fa fa-comments-o"></i> Activity</h4>
 <?php 
 echo do_shortcode('[widgets_on_pages id="Member Feed"]');
 ?>
+
 </div>
 
 <?php
@@ -61,7 +73,7 @@ echo '<div id="Featured_Content">';
 echo '<h2><i class="ico fa fa-star-o"></i> Featured Content</h2>';
 foreach($post_types as $type){
 
-do_shortcode('[profile_favourites slug="'.$type.'"]');
+do_shortcode('[profile_favourites slug="'.$type.'" user="admin"]');
 //echo '<button class="profile-bookmark-item btn btn-success">See All</button>';
 
 }
