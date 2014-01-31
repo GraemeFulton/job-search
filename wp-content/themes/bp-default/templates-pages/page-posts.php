895e0506a2190c734014683104108882a3a6f880
Graeme <Graemefulton89@gmail.com>
2014-01-31 22:53:30 +0000

    Blog page animations fixed
    Former-commit-id: 8d7c62c51483073e53682e9c1ae5719029bc00f5

--- wp-content/themes/bp-default/templates-pages/page-posts.php
+++ wp-content/themes/bp-default/templates-pages/page-posts.php
@@ -5,8 +5,8 @@
  * A Page for courses
 */
 
-do_action('enable_isotopes');
 include (TEMPLATEPATH . '/templates-headers/header-posts.php');
+do_action('enable_isotopes');
 
 ?>
 <div id="page-container" class="blog-page-container">
@@ -105,11 +105,9 @@ $ratings= show_ratings($post_id);
           <div class="item">
               		<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>
           <?php if($url){?>
-    			<div class="post_image">
-    			<div class="course_post_image">
-<img src="<?php echo $url; ?>" longdesc="URL_2" alt="Text_2" />
-    			</div>
-                 </div>
+    		                                     <div class="post_image post_image_<?php echo $post_id?> is-loading">
+                                            <img class="post_post_image advert_image" src="<?php echo $url;?>"/> 
+                                         </div>
 <?php }?>
 			<h2 class="posttitle"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'buddypress' ); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
