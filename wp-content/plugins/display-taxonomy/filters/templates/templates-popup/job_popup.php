107b67e7868709179fd9f05a257e3a54c4d88a60
Graeme Fulton <graemefulton89@gmail.com>
2014-02-01 00:34:40 +0000

    job expiry dates
    Former-commit-id: 3fced898ae1703b5d64dd8c3a2980e98cb22f8fb

--- wp-content/plugins/display-taxonomy/filters/templates/templates-popup/job_popup.php
+++ wp-content/plugins/display-taxonomy/filters/templates/templates-popup/job_popup.php
@@ -11,7 +11,21 @@ $location= $this->get_jobs_location($tree);
         <tr class="alt"><td>Job Type: </td><td><?php echo $post_type;?></td></tr>
         <tr><td>Job Provider:</td><td><img style="float:left; position:relative; max-height:35px; max-width:80px;" src="<?php echo $provider['src']?>"/></td></tr>
         <tr class="alt"><td>Excerpt: </td><td><?php echo $excerpt;?></td></tr>
-        <tr><td>Rating: </td><td><?php echo $ratings;?></td></tr>
+        <tr><td>Date Posted: </td><td><?php echo $post_date=get_the_time('Y-m-d', $this->post_id);?></td></tr>
+        <?php 
+        $date= strtotime("+30 days", strtotime($post_date)); $expiry=date("Y-m-d", $date);
+        
+        list($d,$m,$y)=explode("/",$datestring);
+        $date=mktime(0,0,0,$m,$d,$y);
+        
+        $today=mktime(0,0,0,date("m"),date("d"),date("Y"));
+            
+        if($date>$today)
+        {
+            ?><tr class="alt"><td>Display Until: </td><td><span class="job-expiry">Expired!</span></td></tr><?php
+         
+        }else{ ?><tr class="alt"><td>Display Until: </td><td><?php echo $expiry;?></td></tr><?php } ?>
+        
     </table>
 </div>
 <?php echo '<a class="btn btn-success btn-large" href="'.$link.'">Apply Now</a>';?>
