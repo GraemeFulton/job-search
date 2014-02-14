6d06fbdd13120d17935f639162a85a3999378ffe
Graeme Fulton <graemefulton89@gmail.com>
2014-02-14 03:30:58 +0000

    last changes, social login
    Former-commit-id: 65b75c4aab3a7dfe8aa93d353eeb65ebf440a132

--- wp-content/themes/bp-default/templates-headers/header-inspire.php
+++ wp-content/themes/bp-default/templates-headers/header-inspire.php
@@ -63,7 +63,14 @@
                                 </div>
                                
                                 </form>
-                                                               
+                                       <?php if ( function_exists( 'display_taxonomy_tree' ) ) 
+                                          {
+                                              $tree= display_taxonomy_tree('topic', 'inspire-tag');
+                                                
+                                           //   $tree->display_main_select2_box('search');
+                                          }
+                                    ?>
+                                
                             </div>
 			</div><!-- #search-bar -->
             
@@ -89,18 +96,17 @@
 		<?php do_action( 'bp_before_container' ); ?>
                   <div class="slider-button fixed"></div>
 
-
-                     <div id="search-bar" class="mobile-search mobile-menu" role="search">
+  <div id="search-bar" class="mobile-search mobile-menu" role="search">
                             <div id="main_search">
                                 <form class="navbar-form" role="search">
                                     <div class="input-group">
-                                     <input type="text" class="form-control" id='Search_Term' placeholder="Search for Inspiration" name="srch-term">
+                                     <input type="text" class="form-control" id='Search_Term' placeholder="Search for Posts" name="srch-term">
                                         <div class="input-group-btn">
                                         <button class="btn btn-default" id='Search_Filter'type="submit"><i class="glyphicon glyphicon-search"></i></button>
                                     </div>
                                 </div>
                                
                                 </form>
-                                                               
+                                                                
                             </div>
 			</div><!-- #search-bar -->
