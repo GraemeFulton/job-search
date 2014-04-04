c125913c1cfb0f7a358bd7f81d728307376af470
Graeme Fulton <graemefulton89@gmail.com>
2014-04-04 21:56:23 +0100

    added image to rss feed
    Former-commit-id: 30a92c62b9d978f104c60432f7ac23852e4fb95b

---
+++ wp-content/plugins/add-featured-image-to-rss-feed/add-featured-image-to-rss-feed.php
@@ -0,0 +1,33 @@
+<?php
+/* 
+Plugin Name: Add featured image to RSS feed
+Plugin URI: http://www.tacticaltechnique.com/wordpress/
+Description: Adds the featured image attached to your posts to the RSS feed.
+Author: Corey Salzano
+Version: 1.0
+Author URI: http://twitter.com/salzano
+*/
+
+	function add_featured_image_to_feed($content) {
+		global $post;
+		if ( has_post_thumbnail( $post->ID ) ){
+			$content = '' . get_the_post_thumbnail( $post->ID, 'large' ) . '' . $content;
+		}
+		return $content;
+	}
+
+	add_filter('the_excerpt_rss', 'add_featured_image_to_feed', 1000, 1);
+	add_filter('the_content_feed', 'add_featured_image_to_feed', 1000, 1);
+        
+        
+                add_action( 'rss2_item', 'add_featured_image_to_feed_as_node' );
+
+        function add_featured_image_to_feed_as_node() {
+            
+            global $post;
+            $image = get_the_image($post->ID);
+            echo '<image>'.$image.'</image>';
+            
+        }
+        
+?>
