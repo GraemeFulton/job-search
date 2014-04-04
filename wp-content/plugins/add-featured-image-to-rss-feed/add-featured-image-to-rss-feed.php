00b2a1b94a8341831cf259329223ea31ee2addb1
Graeme Fulton <graemefulton89@gmail.com>
2014-04-04 21:56:53 +0100

    rss image
    Former-commit-id: 2533f5f61025a31053ae3c032c51f46175a5e1b2

--- wp-content/plugins/add-featured-image-to-rss-feed/add-featured-image-to-rss-feed.php
+++ wp-content/plugins/add-featured-image-to-rss-feed/add-featured-image-to-rss-feed.php
@@ -8,16 +8,16 @@ Version: 1.0
 Author URI: http://twitter.com/salzano
 */
 
-	function add_featured_image_to_feed($content) {
-		global $post;
-		if ( has_post_thumbnail( $post->ID ) ){
-			$content = '' . get_the_post_thumbnail( $post->ID, 'large' ) . '' . $content;
-		}
-		return $content;
-	}
-
-	add_filter('the_excerpt_rss', 'add_featured_image_to_feed', 1000, 1);
-	add_filter('the_content_feed', 'add_featured_image_to_feed', 1000, 1);
+//	function add_featured_image_to_feed($content) {
+//		global $post;
+//		if ( has_post_thumbnail( $post->ID ) ){
+//			$content = '' . get_the_post_thumbnail( $post->ID, 'large' ) . '' . $content;
+//		}
+//		return $content;
+//	}
+//
+//	add_filter('the_excerpt_rss', 'add_featured_image_to_feed', 1000, 1);
+//	add_filter('the_content_feed', 'add_featured_image_to_feed', 1000, 1);
         
         
                 add_action( 'rss2_item', 'add_featured_image_to_feed_as_node' );
