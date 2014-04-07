0e94f4a8c98b1aa659ed9c08823f686e36ed5a36
Graeme Fulton <graemefulton89@gmail.com>
2014-04-07 11:00:53 +0100

    execute scraper
    Former-commit-id: 7880a36da4a1204b17f3a02e39639a434d49ef75

---
+++ Lostgrad/execute.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'ScraperBundle/Indeed/graduate-jobs.php'; 
+
+scrape('graduate_job', 'Graduate Scheme');
+scrape('graduate_job', 'Entry Level');
+
+scrape('work_experience', 'Volunteer');
+scrape('work_experience', 'Internship');
+scrape('work_experience', 'Student Jobs');
+
+?>
