891c189c15f1941321ef5bd2ad87fcb4eed54acf
Graeme Fulton <graemefulton89@gmail.com>
2014-04-07 11:06:31 +0100

    adding required files
    Former-commit-id: 640a3ba9f1aa41c6cf304b5c7a8116714c15148c

--- Lostgrad/execute.php
+++ Lostgrad/execute.php
@@ -2,6 +2,15 @@
 
 require 'ScraperBundle/Indeed/graduate-jobs.php'; 
 
+require "SiteBundle/Job.php";;
+
+# create and load the HTML
+require "ScraperBundle/ScraperAbstract.class.php"; 
+require "ScraperBundle/JobScraperAbstract.class.php";
+//require "./IndeedScraper.class.php";
+require "ScraperBundle/Indeed/IndeedPostsGenerator.class.php";
+
+
 scrape('graduate_job', 'Graduate Scheme');
 scrape('graduate_job', 'Entry Level');
 
