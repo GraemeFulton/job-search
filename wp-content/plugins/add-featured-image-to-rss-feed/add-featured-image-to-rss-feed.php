5d289c1f0a38e5a5b5e90f2893e1a374354e62d2
Graeme Fulton <graemefulton89@gmail.com>
2014-04-08 11:17:43 +0100

    RSS Feed altered
    Former-commit-id: 63727045684cd9144830cbc2b989bb2ef4cf672f

--- wp-content/plugins/add-featured-image-to-rss-feed/add-featured-image-to-rss-feed.php
+++ wp-content/plugins/add-featured-image-to-rss-feed/add-featured-image-to-rss-feed.php
@@ -19,15 +19,65 @@ Author URI: http://twitter.com/salzano
 //	add_filter('the_excerpt_rss', 'add_featured_image_to_feed', 1000, 1);
 //	add_filter('the_content_feed', 'add_featured_image_to_feed', 1000, 1);
         
-        
-                add_action( 'rss2_item', 'add_featured_image_to_feed_as_node' );
+            add_action('rss2_item','add_job_details_to_RSS');
+            
+            function add_job_details_to_RSS(){
+                
+                global $post;
+                $post_type=get_post_type( $post->ID );
+                
+                if($post_type=='graduate-job'){
+                   
+                    $job_tree=display_taxonomy_tree('profession', 'company');
 
-        function add_featured_image_to_feed_as_node() {
-            
-            global $post;
-            $image = get_the_image($post->ID);
-            echo '<image>'.$image.'</image>';
-            
-        }
+                    //show image in feed
+                    $image = get_the_image($post->ID);
+                    echo '<image>'.$image.'</image>';
+                    
+                    //show job source
+                    $source = $job_tree->get_linked_taggroup_or_tag($this->post_id, '', ''); 
+                    if($source){
+                      echo '<source>'.$source.'</source>';
+                    }
+                    
+                    //show location
+                    $location = $job_tree->get_taxonomy_field($post->ID, 'location');
+                    if($location){
+                        echo '<location>'.$location.'</location>';
+                    }
+                    
+                    //show profession
+                    $profession = $job_tree->grouped_taxonomy_name($post->ID);
+                    if ($profession){
+                        echo '<profession>'.$profession.'</profession>';
+                    }                    
+                    
+                    
+                }
+                
+            }
+
+
+
+//                add_action( 'rss2_item', 'add_featured_image_to_feed_as_node' );
+//
+//        function add_featured_image_to_feed_as_node() {
+//            
+//            global $post;
+//            $image = get_the_image($post->ID);
+//            echo '<image>'.$image.'</image>';
+//            
+//        }
+//        
+//                 add_action( 'rss2_item', 'add_source_to_rss' );
+//
+//        function add_source_to_rss(){
+//            
+//            global $post;
+//            $image = get_the_image($post->ID);
+//            echo '<source>'.$image.'</source>';
+//            
+//        }
+//        
         
 ?>
