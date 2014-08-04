28f8738278ff530eb9b92ea5f8dba8f7506724a9
Graeme Fulton <graemefulton@uk.ibm.com>
2014-08-03 23:20:02 -0500

    removed header bar
    Former-commit-id: b371393aa2e6455bf93627c4e2219aa0c99db6d3

--- wp-content/themes/klein-2.0.9/header.php
+++ wp-content/themes/klein-2.0.9/header.php
@@ -41,70 +41,7 @@
 <?php //BOOTSTRAP NAV-WALKER
          do_action('bp_after_header');
  ?>
-    <div id="bp-klein-top-bar">
-                <nav id="bp-klein-user-bar" class="container" role="navigation">
-                        <div class="row">
-                                <div class="col-md-8 col-sm-8">
-                                        <p class="remove-bottom site-description">
-                                                <?php bloginfo('description'); ?>
-                                        </p>
-                                </div>
-                                <div id="bp-klein-user-bar-action" class="col-md-4 col-sm-4">
-                                        <div class="pull-right">
-      <?php if( is_user_logged_in() ){ ?>
-                                                        <?php
-                                                                global $current_user;
-                                                                get_currentuserinfo();
-                                                                $user_login = $current_user->user_login;
-                                                        ?>
-                                                        <?php if( function_exists('bp_core_get_user_domain' ) ){ ?>
-                        <a id="klein-top-bp-profile-link" class="btn btn-primary" href="<?php echo bp_core_get_user_domain( $current_user->ID );?>" title="<?php _e('My Profile','klein'); ?>">
-                                                                        <?php echo $current_user->display_name; ?>
-                                                                </a>
-                                                                <?php klein_user_nav(); ?>
-                                                        <?php } ?>
-                                                <?php }else{ ?>
-<a data-toggle="modal" id="klein-login-btn" class="btn btn-primary" href="#klein_login_modal" title="<?php _e( 'Login', 'klein' ); ?>"><i class="icon-lock"></i> <?php _e( 'Login', 'klein' ); ?></a>
-                                        <?php echo str_replace( '<a', '<a id="klein-register-btn" title="'.__('Register','klein').'" class="btn btn-primary" ', wp_register('', '', false)); ?>
-                                                        <div class="modal fade" id="klein_login_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                                                <div class="modal-dialog">
-                                                                        <div class="modal-content">
-                                                                                <div class="modal-header">
-                                                                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                                                                        <h4 class="modal-title">
-                                                                                                <?php _e( sprintf( 'Login to %s', get_bloginfo( 'name' ) ),'klein' ); ?>
-                                                                                        </h4>
-                                                                                </div>
-                                                                                <div class="modal-body">
-                                                                                        <?php do_action( 'klein_before_login_form_modal_body' ); ?>
-<div class="modal-social">
-                                                                                <div class="new-fb-btn new-fb-4 new-fb-default-anim">
-<div class="new-fb-4-1">
-<div class="new-fb-4-1-1">
-<a href="http://lostgrad.com/login/?loginFacebook=1&amp;redirect=http://localhost/LGWP" onclick="window.location = 'http://lostgrad.com/login/?loginFacebook=1&amp;redirect='+window.location.href; return false;">Log In with facebook</a></div>
-</div>
-</div>
-&nbsp;
-
-<div class="new-twitter-btn new-twitter-4 new-twitter-default-anim">
-<div class="new-twitter-4-1">
-<div class="new-twitter-4-1-1"><a href="http://lostgrad.com/login/?loginTwitter=1&amp;redirect=http://localhost/LGWP" onclick="window.location = 'http://lostgrad.com/login/?loginTwitter=1&amp;redirect='+window.location.href; return false;">Log In with twitter</a></div>
-</div>
-    </div>
-</div>
-                                                                                                <?php wp_login_form(); ?>
-                                                                                        <?php do_action( 'klein_after_login_form_modal_body' ); ?>
-                                                                                </div>
-                                                                        </div><!-- /.modal-content -->
-                                                                </div><!-- /.modal-dialog -->
-                                                        </div><!-- /.modal -->
-                                                <?php } ?>
-                                        </div>
-
- </div>
-                        </div>
-                </nav>
-        </div>
+   
     
   <div class="clearfix"></div>
 
