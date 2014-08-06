f3eed330a20f19d7e34e9084e808436016c163cf
Graeme Fulton <graemefulton@uk.ibm.com>
2014-08-05 21:00:42 -0500

    homepage
    Former-commit-id: 97b15aede64133d0e2600f701fe92e5c900096fc

--- wp-content/themes/klein-2.0.9/page-templates/front-page-lg.php
+++ wp-content/themes/klein-2.0.9/page-templates/front-page-lg.php
@@ -11,5 +11,50 @@
         echo do_shortcode($content);
         
         ?>
-	
+<center><h1 style="margin-top:12px">Discover the Opportunities.</h1></center>
+	<?php
+        
+        $post_types=array(
+            array("course", "Learn", '#17ABFF'),
+            array("work-experience-job", "Experience", '#E468E4'),
+            array("graduate-job", "Work", '#FF8627'),
+            array("travel-opportunities", "Travel", '#57BD57'),
+            array("inspire-posts", "Inspire", '#EEBA38')
+            );
+        
+        foreach($post_types as $post_type){
+        echo'<div class="home-box-container">';
+            echo get_title($post_type[0]);
+        $myposts = get_posts(array(
+            'showposts' => 3,
+            'post_type' => $post_type[0],
+            'orderby'=>'rand'
+        ));
+            foreach ($myposts as $mypost) {
+                $img=get_the_image($mypost->ID);
+                $link=get_permalink($mypost->ID);
+
+                  echo '<a href="'.$link.'"><div class="home-page-box" style="border-bottom:2px solid '.$post_type[2].'"><img class="home-page-images" src="'.$img.'">';
+                  echo '<p>'.$mypost->post_title . '</p></div></a>';
+
+            }
+                    
+         echo'</div>';
+
+        }
+        
+        
+        function get_title($post_type){
+            echo'<center><h3>';
+            if($post_type=="course")echo 'Learn';
+            elseif($post_type=="graduate-job")echo 'Work';
+            elseif($post_type=="work-experience-job")echo 'Experience';
+            elseif($post_type=="travel-opportunities")echo 'Travel';
+            elseif($post_type=="inspire-posts")echo 'Inspire';
+            
+            echo'</h3></center>';
+
+        }
+        
+?>
 <?php get_footer(); ?>
