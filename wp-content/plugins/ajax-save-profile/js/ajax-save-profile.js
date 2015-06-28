285c5592b964c8d148447bda024ec0b97e8ee831
Graeme Fulton <graemefulton89@gmail.com>
2015-06-28 14:49:30 +0100

    changed class of submit button

--- wp-content/plugins/ajax-save-profile/js/ajax-save-profile.js
+++ wp-content/plugins/ajax-save-profile/js/ajax-save-profile.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 	
-	alert('hi')
+	$('.ajax-submit').click(function(e){
+		
+		//prevent form submitting
+		e.preventDefault();
+		
+		//async save
+		
+		$.ajax({
+			type:'post',
+			url:ajax_var.url,
+			data: {
+				'action':'ajax_save'
+				},
+			success:function(data){
+				
+				console.log(data);
+				
+			}
+		
+			
+			
+		})
+		
+		
+		
+		
+	});
 	
 })
