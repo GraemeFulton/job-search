44dc138b4a613ff186181976ef6568fddedf8d05
Graeme Fulton <graemefulton89@gmail.com>
2015-06-28 23:26:19 +0100

    completed ajax profile saves

--- wp-content/plugins/ajax-save-profile/ajax-save-profile.php
+++ wp-content/plugins/ajax-save-profile/ajax-save-profile.php
@@ -16,15 +16,82 @@ add_action('wp_ajax_nopriv_ajax_save', 'ajax_save');
 
 function ajax_save(){
 	
-	
+	global $current_user;
+	global $bp;
+	get_currentuserinfo();
 	
 	$obj = stripslashes($_POST['selected']);
-	$obj = json_decode($obj);
-	var_dump($obj);
+	$object = json_decode($obj);
+	
+	//var_dump($object);
+	
+	foreach ($object as $item) {
+		
+		$item->value = str_replace('&', '&amp;', $item->value);
+			
+						
+			//set the data
+			if($item->value=='blank'){
+				$item->value='';
+			}
+			$grp = explode(" ", $item->group);
+			$id = explode("_", $grp[1]);
+			$item->group = $id[1];
+			
+			if($item->group==null){
+				$grp = explode('_', $item->id); 
+				$item->group=$grp[1];
+			}
+		
+		//xprofile_set_field_data($field_id, $current_user->id, $item->value);
+			
+		
+		
+	}
+	
+	var_dump($object);
+	$new_array = array();
+	foreach ($object as $item)
+	    $new_array[$item->group][] = $item;
+	  	
 	
 	
+	foreach($new_array as $key => $items){
+		
+		$insert= array();
+		foreach($items as $item){
+			
+			array_push($insert, $item->value);
+			
+		}
+// 		var_dump($key);
+//		var_dump($insert);
+		$putin=array();
+		foreach($insert as $ins){
+			array_push($putin,$ins);
+			
+			
+		}
+		var_dump($putin);
+		xprofile_set_field_data($key, $current_user->id,$putin);
+		
+	}
+	
+	echo 'done';
 
 	exit();
+}
+
+function array_2d_to_1d ($input_array) {
+    $output_array = array();
+
+    for ($i = 0; $i < count($input_array); $i++) {
+      for ($j = 0; $j < count($input_array[$i]); $j++) {
+        $output_array[] = $input_array[$i][$j];
+      }
+    }
+
+    return $output_array;
 }
 
 ?>
--- wp-content/plugins/ajax-save-profile/js/ajax-save-profile.js
+++ wp-content/plugins/ajax-save-profile/js/ajax-save-profile.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+	$(':checkbox:not(:checked)').attr('value', 'blank');
+	
+	$(':checkbox').change(function() {
 
+    $(this).val($.trim($(this).parent().text()))
+    $(':checkbox:not(:checked)').attr('value', 'blank');
+	}); 
+	
 	$('.ajax-submit').click(function(e){
 		
 		//prevent form submitting
@@ -8,7 +15,6 @@ $(document).ready(function(){
 		//populate fields
 		var opts = populate_ajax_fields();
 		var json_data = JSON.stringify(opts, null, 2);
-console.log(json_data)
 
 		//async save
 		$.ajax({
@@ -42,13 +48,46 @@ console.log(json_data)
 function populate_ajax_fields(){
 	var selected=[];
 
+//	$('.standard-form').each(function(){
+//		var checkbox= $(this).find('.checkbox')
+//		if(checkbox[0] != null){
+//		var options = $(this).find('.checkbox input').serializeArray();
+//		}
+//		else{
+//			var options = $(this).find('input[type="text"]').serializeArray();
+//		}
+//		options.unshift({group:$(this).attr('data-group')})
+//		selected.push(options);
+//		
+//	})
+	
 	$('.standard-form').each(function(){
-		
-		var options = $(this).find('.checkbox input:checked').serializeArray()
-		options.push({group:$(this).attr('data-group')})
-		selected.push(options);
+		var checkbox= $(this).find('.checkbox')
+		if(checkbox[0] != null){
+			$(this).find('.checkbox input').each(function(){
+				var field_id =$(this).attr('id');
+				var val = $(this).val(); 
+				var grp = $(this).parents('.editfield').attr('class');
+				
+				selected.push({id:field_id,value:val, group:grp });
+				
+			})
+		}
+		else{
+			 $(this).find('input[type="text"]').each(function(){
+				 var text= $(this).val();
+				 var field_id = $(this).attr('id');
+				 var grp = field_id;
+				 
+					selected.push({id:field_id, value:text, group:grp})
+
+			})
+			
+
+		}
 		
 	})
+	
 	
 	return selected;
 	
