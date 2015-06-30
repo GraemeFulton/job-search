164d5c5ecccbd5d128266a3dcbe6faafa6714154
Graeme Fulton <graemefulton89@gmail.com>
2015-06-30 22:58:49 +0100

    only load ajax-profile saving scripts on profile page

--- wp-content/plugins/ajax-save-profile/ajax-save-profile.php
+++ wp-content/plugins/ajax-save-profile/ajax-save-profile.php
@@ -7,12 +7,19 @@ Version: 1.0
 Author: Graeme Fulton
 Author URI: http://gfulton.me.uk
 */
-wp_register_script('ajax-save-profile', plugins_url('js/ajax-save-profile.js', __FILE__), array( 'jquery' ));
-wp_enqueue_script('ajax-save-profile');
-wp_localize_script('ajax-save-profile', 'ajax_var', array('url'=>admin_url('admin-ajax.php')));
+global $bp;
 
-add_action('wp_ajax_ajax_save', 'ajax_save');
-add_action('wp_ajax_nopriv_ajax_save', 'ajax_save');
+function ajax_save_main(){
+if(bp_is_my_profile()){
+	wp_register_script('ajax-save-profile', plugins_url('js/ajax-save-profile.js', __FILE__), array( 'jquery' ));
+	wp_enqueue_script('ajax-save-profile');
+	wp_localize_script('ajax-save-profile', 'ajax_var', array('url'=>admin_url('admin-ajax.php')));
+	
+	add_action('wp_ajax_ajax_save', 'ajax_save');
+	add_action('wp_ajax_nopriv_ajax_save', 'ajax_save');
+	}
+}
+add_action( 'bp_include', 'ajax_save_main' );
 
 function ajax_save(){
 	
--- wp-content/themes/buddy-boot/header.php
+++ wp-content/themes/buddy-boot/header.php
@@ -50,4 +50,3 @@
 	
 		
 </header><!-- #masthead .site-header -->
-
