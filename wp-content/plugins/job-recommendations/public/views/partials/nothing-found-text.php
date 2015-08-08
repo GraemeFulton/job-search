d7b88696686a2fb716b878223f7b8fd4d521847b
Graeme Fulton <graemefulton89@gmail.com>
2015-08-08 23:03:02 +0100

    no results template

---
+++ wp-content/plugins/job-recommendations/public/views/partials/nothing-found-text.php
@@ -0,0 +1,3 @@
+<article class="post container">
+ 	<p class="404">We've found <?php echo $wp_query->found_posts;?> jobs, but none in your preferred location, what about these?</p>
+</article> 
--- wp-content/plugins/job-recommendations/public/views/partials/secondary-job-loop.php
+++ wp-content/plugins/job-recommendations/public/views/partials/secondary-job-loop.php
@@ -13,11 +13,7 @@ if ( $wp_query->have_posts() ) :
 // Do we have any posts/pages in the databse that match our query?
 
 if($current_post_number ==-1){
-	?>
-	<article class="post container">
- 	<p class="404">We've found <?php echo $wp_query->found_posts;;?> jobs, but none in your preferred location, what about these?</p>
-</article> 
-	<?php 
+	include('nothing-found-text.php');
 }
 ?>
 			
@@ -32,8 +28,8 @@ if($current_post_number ==-1){
 				<?php endwhile; // OK, let's stop the page loop once we've displayed it ?>
 
 			<?php else : // Well, if there are no posts to display and loop through, let's apologize to the reader (also your 404 error) 
-			include('../template-job-none-found.php');
-				
+			include('nothing-found-text.php');
+			
 			endif; // OK, I think that takes care of both scenarios (having a page or not having a page to show)
                     
 ?>
