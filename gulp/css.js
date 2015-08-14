3f4e1e30d0ef15457b83ac4d472e26edc709f9b2
Graeme Fulton <graemefulton89@gmail.com>
2015-08-14 14:19:23 +0100

    liverreload

--- gulp/css.js
+++ gulp/css.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+
 
 gulp.task('landingpage', function(){
 
 	gulp.src('./wp-content/themes/buddy-boot/page-templates/landing-page-libs/css/*.scss')
 	.pipe(sass().on('error', sass.logError))
 	.pipe(gulp.dest('wp-content/themes/buddy-boot/page-templates/landing-page-libs/css'))
+  .pipe(livereload());
 })
 
 gulp.task('mainstyles', function(){
@@ -13,4 +16,5 @@ gulp.task('mainstyles', function(){
 	gulp.src('./wp-content/themes/buddy-boot/*.scss')
 	.pipe(sass().on('error', sass.logError))
 	.pipe(gulp.dest('./wp-content/themes/buddy-boot'))
+  .pipe(livereload());
 })
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 
 var gulp = require('gulp');
-
+var livereload = require('gulp-livereload');
 /**
  * Read in all the files under /gulp
  */
@@ -15,6 +15,8 @@ fs.readdirSync(__dirname + '/gulp').forEach(function(task){
  */
 
 gulp.task('watch:css', ['landingpage', 'mainstyles'], function(){
+
+	livereload.listen();
 
 	gulp.watch('./wp-content/themes/buddy-boot/page-templates/landing-page-libs/css/*.scss', ['landingpage']);
 	gulp.watch('./wp-content/themes/buddy-boot/*.scss', ['mainstyles']);
