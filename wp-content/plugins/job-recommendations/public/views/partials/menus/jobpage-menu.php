4b6b6a6d44e3532d0561dec5b2a0e4b74b838b2d
Graeme Fulton <graemefulton89@gmail.com>
2015-08-17 22:35:28 +0100

    hierarchical categories

--- wp-content/plugins/job-recommendations/public/views/partials/menus/jobpage-menu.php
+++ wp-content/plugins/job-recommendations/public/views/partials/menus/jobpage-menu.php
@@ -1,6 +1,6 @@
 <?
 $jobmenu=false;
-if(strpos($_SERVER['REQUEST_URI'],'company')||strpos($_SERVER['REQUEST_URI'],'job-roll')||strpos($_SERVER['REQUEST_URI'],'graduate-job')||strpos($_SERVER['REQUEST_URI'],'?s=') ){
+if(strpos($_SERVER['REQUEST_URI'],'company')||strpos($_SERVER['REQUEST_URI'],'job-roll')||strpos($_SERVER['REQUEST_URI'],'graduate-job')||strpos($_SERVER['REQUEST_URI'],'?s=')||strpos($_SERVER['REQUEST_URI'],'profession') ){
 
     $jobmenu = true;
 }
@@ -18,16 +18,36 @@ if($jobmenu==true){
   $professions = get_terms('profession');
   foreach($professions as $profession)
   {
-    ?>
-      <li class="">
-        <a href="#dropdown1" data-toggle="tab" aria-expanded="true"><?php echo $profession->name;?>
-        <div class="ripple-wrapper">
-          <div class="ripple ripple-on ripple-out"></div>
-        </div>
-      </a>
-    </li>
-    <li class="divider"></li>
-    <?php
+    if($profession->parent==0)//if there is no parent, it is a top-level category
+    {
+      ?>
+        <li style="float:left">
+          <a href="<?php echo get_term_link( $profession ); ?>" aria-expanded="true"><?php echo $profession->name;?>
+          <div class="ripple-wrapper">
+            <div class="ripple ripple-on ripple-out"></div>
+          </div>
+        </a>
+      </li>
+      <li class="divider"></li>
+      <?php
+        //get term children
+         $termchildren= get_term_children( $profession->term_id, 'profession' );
+         foreach($termchildren as $child)
+         {
+            $term = get_term_by( 'id', $child, 'profession');
+            ?>
+              <li style="float:left">
+                <a href="<?php echo get_term_link( $term ); ?>" aria-expanded="true"><?php echo $term->name;?>
+                <div class="ripple-wrapper">
+                  <div class="ripple ripple-on ripple-out"></div>
+                </div>
+              </a>
+            </li>
+            <li class="divider"></li>
+            <?php
+         }
+
+    }
 
   }
   echo '</ul>';
