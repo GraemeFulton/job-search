6e817fc3be290a9312968271301185f0da9f9d17
Graeme Fulton <graemefulton89@gmail.com>
2015-08-22 14:02:08 +0100

    layout of account settings

--- wp-content/themes/buddy-boot/buddypress/members/single/settings/general.php
+++ wp-content/themes/buddy-boot/buddypress/members/single/settings/general.php
@@ -5,17 +5,17 @@
 	<?php if ( !is_super_admin() ) : ?>
 
 		<label for="pwd"><?php _e( 'Current Password <span>(required to update email or change current password)</span>', 'buddypress' ); ?></label>
-		<input type="password" name="pwd" id="pwd" size="16" value="" class="settings-input small" /> &nbsp;<a href="<?php echo wp_lostpassword_url(); ?>" title="<?php _e( 'Password Lost and Found', 'buddypress' ); ?>"><?php _e( 'Lost your password?', 'buddypress' ); ?></a>
-
+		<input type="password" name="pwd" id="pwd" size="16" value="" class="settings-input small" placeholder="Enter current password to make changes" /> <br><a href="<?php echo wp_lostpassword_url(); ?>" title="<?php _e( 'Password Lost and Found', 'buddypress' ); ?>"><?php _e( 'Lost your password?', 'buddypress' ); ?></a>
+		<hr>
 	<?php endif; ?>
 
 	<label for="email"><?php _e( 'Account Email', 'buddypress' ); ?></label>
-	<input type="text" name="email" id="email" value="<?php echo bp_get_displayed_user_email(); ?>" class="settings-input" />
-
+	<input type="text" name="email" id="email" value="<?php echo bp_get_displayed_user_email(); ?>" class="settings-input" /><br>
+	<hr>
 	<label for="pass1"><?php _e( 'Change Password <span>(leave blank for no change)</span>', 'buddypress' ); ?></label>
-	<input type="password" name="pass1" id="pass1" size="16" value="" class="settings-input small" /> &nbsp;<?php _e( 'New Password', 'buddypress' ); ?><br />
-	<input type="password" name="pass2" id="pass2" size="16" value="" class="settings-input small" /> &nbsp;<?php _e( 'Repeat New Password', 'buddypress' ); ?>
-
+	<input type="password" name="pass1" id="pass1" size="16" value="" class="settings-input small" placeholder="Enter new password" /> <br>
+	<input type="password" name="pass2" id="pass2" size="16" value="" class="settings-input small" placeholder = "Enter new password again" /><br />
+	<hr>
 	<?php do_action( 'bp_core_general_settings_before_submit' ); ?>
 
 	<div class="submit">
