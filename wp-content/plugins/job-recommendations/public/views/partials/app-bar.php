c3fe88c58993557ed2e7bd3e7828243a68d129b2
Graeme Fulton <graemefulton89@gmail.com>
2015-08-23 16:18:47 +0100

    fix

--- wp-content/plugins/job-recommendations/public/views/partials/app-bar.php
+++ wp-content/plugins/job-recommendations/public/views/partials/app-bar.php
@@ -2,7 +2,7 @@
 <?php if(!is_user_logged_in()){
     $link = get_site_url().'/register';
 }else{
-    $link = get_site_url() .'/members/'.bp_core_get_username( get_current_user_id() ) . '/profile/edit/group/14';
+    $link = get_site_url() .'/members/'.bp_core_get_username( get_current_user_id() ) . '/profile/edit/group/4';
 }
 ?>
 <div class='no-pad box-head col-xs-12 col-md-offset-2 col-md-10'>
