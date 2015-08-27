c7d4f4f26f1ca46872c1be046065376bacfcc605
Graeme Fulton <graemefulton89@gmail.com>
2015-08-27 10:40:26 +0100

    institution n/a

--- wp-content/plugins/display-taxonomy/filters/templates/templates-post-loop/job_post_loop.php
+++ wp-content/plugins/display-taxonomy/filters/templates/templates-post-loop/job_post_loop.php
@@ -2,7 +2,9 @@
 //get location
 global $paged;
 $location= $this->get_jobs_location($tree);
-
+if(!isset($institution)){
+  $institution='N/A';
+}
 //get date
 $post_date=get_the_time('Y-m-d', $this->post_id);
    $now = time(); // or your date as well
