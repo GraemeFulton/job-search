209b40ec6e69ef51dae91048762e7cf53692307e
Graeme Fulton <graemefulton89@gmail.com>
2015-08-27 23:25:34 +0100

    login form

--- wp-content/themes/buddy-boot/functions.php
+++ wp-content/themes/buddy-boot/functions.php
@@ -294,6 +294,11 @@ function myplugin_registration_save(  $user_id, $userdata, $form_id, $form_setti
         .login h1 a {
             display:none;
         }
+				#login{
+					padding: 15px;
+					background: #fff;
+					margin: 100px;
+				}
     </style>
 <?php }
 add_action( 'login_enqueue_scripts', 'my_login_logo' );
